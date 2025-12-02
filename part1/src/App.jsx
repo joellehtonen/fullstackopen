4965bd1b3511fc805f3037f959d9d5102a784d4a
Joel <joel.v.lehtonen@gmail.com>
2025-12-02 19:15:53 +0200

    next step

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -7,17 +7,10 @@ const Hello = () => {
 }
 
 const App = () => {
-    console.log('Hello from component')
-    const now = new Date()
-    const a = 10
-    const b = 20
-    console.log(now, a+b);
     return (
         <div>
-            <p>Hello world, it is {now.toString()}</p>
-            <p>
-                {a} plus {b} is {a + b}
-            </p>
+            <h1>Greetings</h1>
+            <Hello />
         </div>
     )
 }
