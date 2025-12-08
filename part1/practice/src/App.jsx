70c5267ab51a7d0604445da60e2e5a1eeabd8ea0
Joel <joel.v.lehtonen@gmail.com>
2025-12-08 20:14:14 +0200

    slight refactoring

--- part1/practice/src/App.jsx
+++ part1/practice/src/App.jsx
@@ -1,17 +1,30 @@
 import { useState } from 'react'
 
+const Display = ({ counter }) => {
+    return (
+        <div>{counter}</div>
+    )
+}
+
+const Button = ({ onClick, text}) => {
+    return (
+        <button onClick={onClick}>{text}</button>
+    )
+}
+
 const App = () => {
     const [counter, setCounter] = useState(0)
 
-    // const handleClick = () => {
-    //     console.log('clicked')
-    // }
+   const increaseByOne = () => setCounter(counter + 1)
+   const setToZero = () => setCounter(0)
+   const decreaseByOne = () => setCounter(counter - 1)
 
     return (
         <>
-            <div>{counter}</div>
-            <button onClick={() => setCounter(counter + 1)}>plus</button>
-            <button onClick={() => setCounter(0)}>zero</button>
+            <Display counter={counter} />
+            <Button onClick={increaseByOne} text='plus' />
+            <Button onClick={decreaseByOne} text='minus' />
+            <Button onClick={setToZero} text='zero' />
         </>
     )
 }
