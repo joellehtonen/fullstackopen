a4bf69d1355dea9c11cfe6baee9422347c30d64a
Joel <joel.v.lehtonen@gmail.com>
2025-12-08 18:57:18 +0200

    counter practice

--- part1/practice/src/main.jsx
+++ part1/practice/src/main.jsx
@@ -3,7 +3,11 @@ import App from './App'
 
 let counter = 1
 
-const root = ReactDOM.createRoot(document.getElementById('root')).render(<App counter={counter}/>)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const refresh = () => {
+    root.render(<App counter={counter}/>)
+}
 
 setInterval(() => {
     refresh()
