d79ee4b9fa0fc1aa6c1dbfd6fa308ce55414e3d7
Joel <joel.v.lehtonen@gmail.com>
2025-12-08 19:09:41 +0200

    counter practice v2

--- part1/practice/src/App.jsx
+++ part1/practice/src/App.jsx
@@ -1,4 +1,10 @@
-const App = ({ counter }) => {
+import { useState } from 'react'
+
+const App = () => {
+    const [counter, setCounter] = useState(0)
+
+    setTimeout(() => setCounter(counter + 1), 1000)
+
     return (
         <div>{counter}</div>
     )
