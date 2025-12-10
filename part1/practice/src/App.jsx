88fd267953714c443971facfded1fc4c545583a1
Joel <joel.v.lehtonen@gmail.com>
2025-12-10 21:19:39 +0200

    added new total component

--- part1/practice/src/App.jsx
+++ part1/practice/src/App.jsx
@@ -11,23 +11,28 @@ const Button = ({ onClick, text}) => <button onClick={onClick}>{text}</button>
 const App = () => {
     const [clicks, setClicks] = useState({left: 0, right: 0})
     const [allClicks, setAllClicks] = useState([])
+    const [total, setTotal] = useState(0)
 
     const handleLeftClick = () => {
-        setAllClicks(allClicks.concat('L'))
+        const newClicks = allClicks
+        const newLeft = clicks.left + 1
+        newClicks.push('L')
+        setAllClicks(newClicks)
         setClicks({
             ...clicks,
-            left: clicks.left + 1
+            left: newLeft
         })
-        console.log('clicked left')
+        setTotal(newLeft + clicks.right)
     }
 
     const handleRightClick = () => {
+        const newRight = clicks.right + 1
         setAllClicks(allClicks.concat('R'))
         setClicks({
             ...clicks,
-            right: clicks.right + 1
+            right: newRight
         })
-        console.log('clicked right')
+        setTotal(clicks.left + newRight)
     }
 
 
@@ -38,6 +43,7 @@ const App = () => {
             <button onClick={handleRightClick}>right</button>
             {clicks.right}
             <p>{allClicks.join('')}</p>
+            <p>total: {total}</p>
         </div>
     )
 }
