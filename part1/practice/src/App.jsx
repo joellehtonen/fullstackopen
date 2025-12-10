28628e0d2c4e04f32da2cecd9f4cb7eec60616fe
Joel <joel.v.lehtonen@gmail.com>
2025-12-10 20:37:21 +0200

    practising complex state

--- part1/practice/src/App.jsx
+++ part1/practice/src/App.jsx
@@ -9,19 +9,32 @@ const Display = ({ counter }) => {
 const Button = ({ onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-    const [counter, setCounter] = useState(0)
+    const [clicks, setClicks] = useState({left: 0, right: 0})
 
-   const increaseByOne = () => setCounter(counter + 1)
-   const setToZero = () => setCounter(0)
-   const decreaseByOne = () => setCounter(counter - 1)
+    const handleLeftClick = () => {
+        const newClicks = {
+            left: clicks.left + 1,
+            right: clicks.right
+        }
+        setClicks(newClicks)
+    }
+
+    const handleRightClick = () => {
+        const newClicks = {
+            left: clicks.left,
+            right: clicks.right + 1
+        }
+        setClicks(newClicks)
+    }
+
 
     return (
-        <>
-            <Display counter={counter} />
-            <Button onClick={increaseByOne} text='plus' />
-            <Button onClick={decreaseByOne} text='minus' />
-            <Button onClick={setToZero} text='zero' />
-        </>
+        <div>
+            {clicks.left}
+            <button onClick={handleLeftClick}>left</button>
+            <button onClick={handleRightClick}>right</button>
+            {clicks.right}
+        </div>
     )
 }
 
