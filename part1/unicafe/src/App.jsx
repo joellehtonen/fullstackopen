2fac226f397b5e6d923dc2909a161461aecc8cc2
Joel <joel.v.lehtonen@gmail.com>
2025-12-11 20:06:16 +0200

    1.7 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,26 +1,55 @@
 import { useState } from 'react'
 
+const Average = ({ score, all }) => {
+    const average = score / all
+    if (all < 1)
+        return (
+            'Average: 0' 
+    )
+    return (
+        'Average: ' + average
+    )
+}
+
+const Positive = ({ good, all }) => {
+    const positive = (good / all) * 100
+    if (all < 1)
+        return (
+            'Positive: '
+    )
+    return (
+        'Positive: ' + positive + '%'
+    )
+}
+
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [all, setAll] = useState(0)
+    const [score, setScore] = useState(0)
 
     const handleGood = () => {
         const updatedGood = good + 1
         setGood(updatedGood)
         console.log('good:', updatedGood)
+        setAll(all + 1)
+        setScore(score + 1)
     }
 
     const handleNeutral = () => {
         const updatedNeutral = neutral + 1
         setNeutral(updatedNeutral)
         console.log('neutral:', updatedNeutral)
+        setAll(all + 1)
     }
 
     const handleBad = () => {
         const updatedBad = bad + 1
         setBad(updatedBad)
         console.log('bad:', updatedBad)
+        setAll(all + 1)
+        setScore(score - 1)
     }
 
     return (
@@ -34,6 +63,10 @@ const App = () => {
             <p>Good: {good}</p>
             <p>Neutral: {neutral}</p>
             <p>Bad: {bad}</p>
+            <p>All: {all}</p>
+            <Average score={score} all={all} />
+            <br /> <br />
+            <Positive good={good} all={all} />
         </div>
     )
 }
