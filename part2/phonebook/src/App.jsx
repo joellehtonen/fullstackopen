edc05e5e46d4507c95287e38dcb9b466f7e01eb6
Joel <joel.v.lehtonen@gmail.com>
2025-12-20 18:38:51 +0200

    ids are now based on name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,23 +3,21 @@ import { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Joel Lehtonen', id: 1 }
+        { name: 'Joel Lehtonen' }
     ])
-    const [newName, setNewname] = useState('')
+    const [newName, setNewName] = useState('')
 
     const handleNameSubmit = (event) => {
-        console.log('in submit')
         event.preventDefault()
         const newPerson = {
-            name: newName, 
-            id: String(persons.length + 1)
+            name: newName
         }
-        console.log('newPerson', newPerson)
         setPersons(persons.concat(newPerson))
+        setNewName('')
     }
     
     const handleNameTyping = (event) => {
-        setNewname(event.target.value)
+        setNewName(event.target.value)
         console.log('typing:', event.target.value)
     }
 
@@ -28,7 +26,7 @@ const App = () => {
             <h2>Phonebook</h2>
             <form onSubmit={handleNameSubmit}>
                 <div>
-                    name: <input onChange={handleNameTyping}/>
+                    name: <input onChange={handleNameTyping} value={newName}/>
                 </div>
                 <div>
                     <button type='submit'>add</button>
@@ -37,7 +35,7 @@ const App = () => {
             <h2>Numbers</h2>
             <ul>
                 {persons.map(person => 
-                    <li key={person.id}>{person.name}</li>
+                    <li key={person.name}>{person.name}</li>
                 )}
             </ul>
         </div>
