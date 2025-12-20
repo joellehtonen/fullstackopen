5f63f071ab02d75a3f51d9b7b0f3464b7ef4ee26
Joel <joel.v.lehtonen@gmail.com>
2025-12-20 18:30:50 +0200

    2.6 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,23 +3,43 @@ import { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Joel Lehtonen' }
+        { name: 'Joel Lehtonen', id: 1 }
     ])
     const [newName, setNewname] = useState('')
+
+    const handleNameSubmit = (event) => {
+        console.log('in submit')
+        event.preventDefault()
+        const newPerson = {
+            name: newName, 
+            id: String(persons.length + 1)
+        }
+        console.log('newPerson', newPerson)
+        setPersons(persons.concat(newPerson))
+    }
+    
+    const handleNameTyping = (event) => {
+        setNewname(event.target.value)
+        console.log('typing:', event.target.value)
+    }
 
     return (
         <div>
             <h2>Phonebook</h2>
-            <form>
-                <div> DEBUG: {newName}</div>
+            <form onSubmit={handleNameSubmit}>
                 <div>
-                    name: <input />
+                    name: <input onChange={handleNameTyping}/>
                 </div>
                 <div>
                     <button type='submit'>add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
+            <ul>
+                {persons.map(person => 
+                    <li key={person.id}>{person.name}</li>
+                )}
+            </ul>
         </div>
     )
 }
