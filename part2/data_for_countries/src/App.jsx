16d21c6eb12f2dd2c3ab1d89c9083b7a5be75efd
Joel <joel.v.lehtonen@gmail.com>
2025-12-30 21:50:22 +0200

    2.18 finally done

---
+++ part2/data_for_countries/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Result from './components/Result'
+
+const App = () => {
+    const [countries, setCountries] = useState([])
+    const [value, setValue] = useState('')
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        axios
+            .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+            .then((response) => {
+                setCountries(response.data)
+            })
+            .catch(
+                setError(true)
+            )
+    }, [])
+    // console.log('countries', countries)
+
+    const handleTyping = (event) => {
+        const data = event.target.value
+        console.log('typing', data)
+        setValue(data)
+    }
+
+    if (error)
+        return <div>Sorry, an error occurred...</div>
+
+    return (
+        <div>
+            <h1>DATA FOR COUNTRIES</h1>
+            <div>
+                Find countries 
+                <input value={value} onChange={handleTyping} />
+            </div>
+            <Result value={value} countries={countries} />
+        </div>
+    )
+}
+
+export default App
---
+++ part2/data_for_countries/src/components/Result.jsx
@@ -0,0 +1,42 @@
+const Result = ({ value, countries }) => {
+    let results
+    if (value) {
+        results = countries.filter(country => country.name.common.toLowerCase().includes(value.toLowerCase()))
+        console.log('RESULTS', results)
+        if (results.length > 10) {
+            return (
+                <div>
+                    Too many matches, please specify
+                </div>
+        )}
+        if (results.length === 1) {
+            return (
+                <div>
+                    <h2>{results[0].name.common}</h2>
+                    <p>Capital: {results[0].capital[0]}</p>
+                    <p>Area: {results[0].area}</p>
+                    <h3>Languages:</h3>
+                    <ul>
+                        {Object.entries(results[0].languages).map(([key, value]) => 
+                            <li key={key}>
+                                {value}
+                            </li>)}
+                    </ul>
+                    <img src={results[0].flags.png} />
+                </div>
+        )}
+        else {
+            return (
+                <ul>
+                    {results.map(country => 
+                        <li key={country.name.common}>{country.name.common}</li>
+                    )}
+                </ul>
+        )}
+        return
+    }
+
+    return null
+}
+
+export default Result
